127922d7b5c1500c29e5c9909197a132f2b78b75
Tapkir Pratiksha Balu <89315015+Pratikshatapkir@users.noreply.github.com>
2022-01-07 19:06:38 +0530

    Add files via upload

---
+++ Ifelseif.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.*;
+
+public class Ifelseif {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(a==b){
+            System.out.println("a is eqal");
+        }
+        else if(a>b){
+                System.out.println("a is greater");
+            }
+            else{
+                System.out.println("a is lesser");
+            }
+        }
+
+    }
+
