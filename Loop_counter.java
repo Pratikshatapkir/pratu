7300abd1fcd03ab97de57d607c413fad7c96360c
Tapkir Pratiksha Balu <89315015+Pratikshatapkir@users.noreply.github.com>
2022-01-10 22:02:30 +0530

    Add files via upload

---
+++ Loop_counter.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Loop_counter {
+    public static void main(String[] args) {
+       // for(int counter=0;counter<10;counter=counter+1 ){
+           // System.out.println(counter+"");
+            //for(int i=0;i<11;i++){
+               //System.out.println(i);
+        int i=0;
+       // while(i<11){
+        //    System.out.println(i);
+         //   i++;
+        do{
+            System.out.println(i);
+            i++;
+            }while(i<11);
+        }
+    }
+
