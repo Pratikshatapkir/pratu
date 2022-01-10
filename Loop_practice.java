e4790a1686a74a1575d3afbaeb480faa9aca9824
Tapkir Pratiksha Balu <89315015+Pratikshatapkir@users.noreply.github.com>
2022-01-10 22:02:50 +0530

    Add files via upload

---
+++ Loop_practice.java
@@ -0,0 +1,11 @@
+package com.company;
+import java.util.*;
+public class Loop_practice {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<11;i++){
+            System.out.println(n*i);
+        }
+    }
+}
