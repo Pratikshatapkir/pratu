b8d4e46c16f647ddce1b1c3ff2ecf85584fc788c
Tapkir Pratiksha Balu <89315015+Pratikshatapkir@users.noreply.github.com>
2022-01-13 13:13:34 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.*;
+public class Factorial {
+    public static void printFactorial(int n){
+        int factorial=1;
+        for(int i=n;i>=1;i--) {
+            factorial = factorial * i;
+        }
+            System.out.println(factorial);
+            return;
+        }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+       // System.out.println(factorial);3
+        printFactorial(n);
+
+    }
+}
