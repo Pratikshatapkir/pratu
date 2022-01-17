c5c91b831c483801310a9c3237e77274fb3ed7b7
Tapkir Pratiksha Balu <89315015+Pratikshatapkir@users.noreply.github.com>
2022-01-17 20:23:14 +0530

    Add files via upload

---
+++ StringLength.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class StringLength {
+    public static void main(String[] args) {
+        String firstName="niranjan";
+        String lastName="tapkir";
+        String fullName=firstName+ " "+lastName;
+        System.out.println(fullName.length());
+    }
+}
