bf1cf48d6c2707d88aa4e303fd03f1d4a3ec4c77
Tapkir Pratiksha Balu <89315015+Pratikshatapkir@users.noreply.github.com>
2022-01-17 20:25:23 +0530

    Add files via upload

---
+++ CompareTo.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class CompareTo {
+    public static void main(String[] args) {
+        String name1="niranjan";
+        String name2="tapkir";
+        if(name1.compareTo(name2)==0){
+            System.out.println("strings are equal");
+        }
+        else{
+            System.out.println("strings are not equal");
+        }
+    }
+}
