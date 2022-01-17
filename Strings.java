3a4877f35b6cb2d8b3d1136d45dc93ea03207932
Tapkir Pratiksha Balu <89315015+Pratikshatapkir@users.noreply.github.com>
2022-01-17 20:21:59 +0530

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,9 @@
+package com.company;
+public class Strings {
+    public static void main(String[] args) {
+        String firstName="niranjan";
+        String lastName="tapkir";
+        String fullName=firstName+ " "+lastName;
+        System.out.println(fullName);
+    }
+}
