45c6965f8e8ac1057ad65dd54d0fa3ddf3bfa526
Tapkir Pratiksha Balu <89315015+Pratikshatapkir@users.noreply.github.com>
2022-01-17 20:26:41 +0530

    Add files via upload

---
+++ CharAt.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class CharAt {
+    public static void main(String[] args) {
+            //public static void main(String[] args) {
+            String firstName="niranjan";
+            String lastName="tapkir";
+            String fullName=firstName+ " "+lastName;
+            System.out.println(fullName.length());
+            for(int i=0;i<fullName.length();i++){
+                System.out.println(fullName.charAt(i));
+            }
+        }
+        }
+
+
+
