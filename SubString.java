8c166f893e0a82f4ae31a3d3e3d2b31e830f067c
Tapkir Pratiksha Balu <89315015+Pratikshatapkir@users.noreply.github.com>
2022-01-17 20:46:56 +0530

    Add files via upload

---
+++ SubString.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class SubString {
+    public static void main(String[] args) {
+        String sentence="tonystark";
+        String name=sentence.substring(4);
+        System.out.println(name);
+    }
+}
+//strings are imutable
