64bcc4ced3ed39b71dbe1adde37fffe8fe7fc749
Tapkir Pratiksha Balu <89315015+Pratikshatapkir@users.noreply.github.com>
2022-02-12 10:12:40 +0530

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int x=10;
+        if(x%2==0){
+            System.out.println(x + " is even no");
+        }
+        else{
+            System.out.println(x +"is odd no");
+        }
+    }
+}
