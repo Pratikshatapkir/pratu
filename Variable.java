9832a80797f682def6e4a6c7084cb70ac043cceb
Tapkir Pratiksha Balu <89315015+Pratikshatapkir@users.noreply.github.com>
2022-02-12 10:12:20 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Variable {
+    public static void main(String[] args) {
+        int a=5;
+        int b=4;
+        int sum=a+b;
+        System.out.println("sum of " + a + " and "+ b + " is " + sum);
+        int pro=a*b;
+        System.out.println(pro);
+    }
+}
