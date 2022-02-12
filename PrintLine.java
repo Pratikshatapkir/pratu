6d7cc036e52a4fc99afcfe16e0c9596ddd0ac001
Tapkir Pratiksha Balu <89315015+Pratikshatapkir@users.noreply.github.com>
2022-02-12 10:12:04 +0530

    Add files via upload

---
+++ PrintLine.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class PrintLine {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("*\n**\n***\n****\n*****");
+
+
+    }
+}
