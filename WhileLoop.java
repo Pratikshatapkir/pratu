833a7fff06416152d2023079df2637da6e740647
Tapkir Pratiksha Balu <89315015+Pratikshatapkir@users.noreply.github.com>
2022-02-14 22:08:37 +0530

    Add files via upload

---
+++ WhileLoop.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+       /* int i=1;
+        while(i<=9){
+            System.out.println(i);
+            i=i+1;
+        }*/
+        for(int i=1;i<=9;i++){
+            System.out.println(i);
+        }
+        System.out.println("done");
+    }
+}
